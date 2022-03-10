32bc67b616a144e61330d2c14eb58ed795eeb266
김범석 <kqjatjr@gmail.com>
2022-03-11 02:06:27 +0900

    공유

--- src/components/Selector/List.js
+++ src/components/Selector/List.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import hotkeys from "hotkeys-js";
 
 const Container = styled.div`
   width: 100%;
@@ -29,12 +30,10 @@ const Item = styled.div`
   display: flex;
   align-items: center;
   text-align: left;
+  background-color: ${(props) => props.background || "white"};
   padding: 10px;
   border-bottom: 1px #e5e7eb solid;
   box-sizing: border-box;
-  :hover {
-    background: #f1f3f8;
-  }
 `;
 
 const List = ({
@@ -45,19 +44,63 @@ const List = ({
   setMultiSelected,
 }) => {
   const [listData, setListData] = useState(data);
+  const [checkValue, setCheckValue] = useState([]);
+  const [multi, setMulti] = useState(true);
+  const [lastClick, setLastClick] = useState();
+
+  const findEl = (el) => listData.find((item) => item.id === Number(el));
+  const onClick = (value) => {
+    setLastClick(value);
+    if (!isHotkeyPressed("command") && !isHotkeyPressed("Shift")) {
+      if (checkValue.includes(Number(value))) {
+        setCheckValue((prev) => prev.filter((item) => item !== Number(value)));
+      } else {
+        setCheckValue((prev) => [...prev, Number(value)]);
+      }
+    }
+    if (multi && isHotkeyPressed("command")) {
+      if (!checkValue.includes(Number(value))) {
+        setCheckValue((prev) => [...prev, Number(value)]);
+      } else {
+        setCheckValue((prev) => prev.filter((item) => item !== Number(value)));
+      }
+    }
+    if (multi && isHotkeyPressed("Shift")) {
+      const firstEl = listData.indexOf(findEl(lastClick));
+      const lastEl = listData.indexOf(findEl(Number(value)));
+      const checkEl = listData
+        .slice(firstEl + 1, lastEl + 1)
+        .reduce((acc, cur) => {
+          return [...acc, cur.id];
+        }, []);
+      if (checkValue.length === 0) {
+        setCheckValue((prev) => prev.concat(checkEl));
+      } else {
+        for (let i = 0; i <= checkEl.length; i++) {
+          if (checkValue.includes(checkEl[i])) {
+            setCheckValue((prev) => prev.filter((item) => item !== checkEl[i]));
+          } else {
+            setCheckValue((prev) => [...prev, Number(checkEl[i])]);
+          }
+        }
+      }
+
+      setLastClick(value);
+    }
+  };
 
   useEffect(() => {
     setListData(data);
   }, [data]);
 
-  const handleOnDragEnd = result => {
+  const handleOnDragEnd = (result) => {
     const items = [...listData];
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
     setListData(items);
   };
 
-  const handleClick = item => {
+  const handleClick = (item) => {
     if (isMoveOneMode) {
       setMultiSelected([item.id]);
     } else {
@@ -71,10 +114,10 @@ const List = ({
       <Title>{title}</Title>
       <SubContainer>
         <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId='fields'>
-            {provided => (
+          <Droppable droppableId="fields">
+            {(provided) => (
               <div
-                className='fields'
+                className="fields"
                 {...provided.droppableProps}
                 ref={provided.innerRef}
                 style={{ width: "100%" }}
@@ -87,13 +130,20 @@ const List = ({
                       draggableId={strFormId}
                       index={index}
                     >
-                      {provided => (
+                      {(provided) => (
                         <div
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
                           ref={provided.innerRef}
                         >
-                          <Item key={item.id} onClick={() => handleClick(item)}>
+                          <Item
+                            key={item.id}
+                            id={item.id}
+                            background={
+                              checkValue.includes(item.id) && "#62D1FF"
+                            }
+                            onClick={(e) => onClick(e.target.id)}
+                          >
                             {item.emoji}&nbsp;&nbsp;
                             {item.name}
                           </Item>
