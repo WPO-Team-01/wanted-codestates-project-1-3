18d7fcfe369debe45458f59d28024e434f8a7e6f
김범석 <kqjatjr@gmail.com>
2022-03-10 23:27:37 +0900

    드래그 기능

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import "./App.css";
-import { moveToSelect, removeFromSelect } from "./redux/contents/contentsSlice";
 import MainPage from "./pages/MainPage";
 
 function App() {
-  const dispatch = useDispatch();
   const data = useSelector((state) => state.contents);
-
-  const asdf = () => {
-    dispatch(moveToSelect([9]));
-  };
-
-  const remove = () => {
-    dispatch(removeFromSelect([9]));
-  };
-
-  console.log(data);
 
   return (
     <div className="App">
       <MainPage data={data} />
-      <button onClick={asdf}>추가</button>
-      <button onClick={remove}>지우기</button>
     </div>
   );
 }
