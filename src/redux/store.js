044244f1444fd896714064991daa3e5ac9ba4f8b
김범석 <kqjatjr@gmail.com>
2022-03-10 15:59:15 +0900

    리듀서 구현

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import contentsReducer from "./contents/contentsSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    contents: contentsReducer,
+  },
+  devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
